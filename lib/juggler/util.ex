05a9ad34d627eb066e6c0b18fc6d3ad154972a30
Alexander Q <qalexx@gmail.com>
2017-03-20 11:28:39 +0300

    chat name for users without username

--- lib/juggler/util.ex
+++ lib/juggler/util.ex
@@ -1,4 +1,5 @@
 defmodule Juggler.Util do
+  def chat_title(%{type: "private", username: nil, first_name: fname, last_name: lname}), do: "@ #{fname} #{lname}"
   def chat_title(%{type: "private", username: uname}), do: "@#{uname}"
   def chat_title(%{title: title}), do: title
 end
