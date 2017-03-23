ee73fca04ac5dc5ab396999793cac9b490b22c9b
Alexander Q <qalexx@gmail.com>
2017-03-23 18:33:02 +0300

    Do not need @boss in Chats

--- lib/juggler/chats.ex
+++ lib/juggler/chats.ex
@@ -1,5 +1,4 @@
 defmodule Juggler.Chats do
-  @boss Application.get_env(:juggler, :boss)
 
   def start_link() do
     initial_state = %{}
@@ -22,20 +21,6 @@ defmodule Juggler.Chats do
 
   def remove_chat(chat) do
     Agent.update(__MODULE__, &Map.delete(&1, chat.id))
-  end
-
-  def boss_chat() do
-    Agent.get(
-      __MODULE__,
-      fn s ->
-        s
-        |> Map.values
-        |> Enum.filter(fn
-          %{type: "private", username: @boss} -> true
-          _ -> false
-        end)
-        |> List.first
-      end)
   end
 
 end
